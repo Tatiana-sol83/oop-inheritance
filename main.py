170d826b0c5e502efc789f29bf47d0a21ce4f5d5
Tatiana-Sol 83 <pugacheva.83@bk.ru>
2026-08-25 15:54:33 +0300

    Добавлены полиморфизм и магические методы

--- main.py
+++ main.py
@@ -21,6 +21,28 @@ class Student:
         else:
             return 'Ошибка'
 
+    def average_grade(self):
+        all_grades = []
+        for grades in self.grades.values():
+            all_grades.extend(grades)
+        if len(all_grades) == 0:
+            return 0
+        return sum(all_grades) / len(all_grades)
+
+    def __str__(self):
+        return (
+            f'Имя: {self.name}\n'
+            f'Фамилия: {self.surname}\n'
+            f'Средняя оценка за домашние задания: {self.average_grade():.1f}\n'
+            f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n'
+            f'Завершенные курсы: {", ".join(self.finished_courses)}'
+        )
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            return 'Ошибка'
+        return self.average_grade() < other.average_grade()
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -33,6 +55,26 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+
+    def average_grade(self):
+        all_grades = []
+        for grades in self.grades.values():
+            all_grades.extend(grades)
+        if len(all_grades) == 0:
+            return 0
+        return sum(all_grades) / len(all_grades)
+
+    def __str__(self):
+        return (
+            f'Имя: {self.name}\n'
+            f'Фамилия: {self.surname}\n'
+            f'Средняя оценка за лекции: {self.average_grade():.1f}'
+        )
+
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            return 'Ошибка'
+        return self.average_grade() < other.average_grade()
 
 
 class Reviewer(Mentor):
@@ -50,18 +92,43 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
 
-lecturer = Lecturer('Иван', 'Иванов')
-reviewer = Reviewer('Пётр', 'Петров')
-student = Student('Алёхина', 'Ольга', 'Ж')
 
-student.courses_in_progress += ['Python', 'Java']
-lecturer.courses_attached += ['Python', 'C++']
-reviewer.courses_attached += ['Python', 'C++']
+student_1 = Student('Ольга', 'Алёхина', 'Ж')
+student_2 = Student('Анна', 'Смирнова', 'Ж')
 
-print(student.rate_lecture(lecturer, 'Python', 7))
-print(student.rate_lecture(lecturer, 'Java', 8))
-print(student.rate_lecture(lecturer, 'С++', 8))
-print(student.rate_lecture(reviewer, 'Python', 6))
+lecturer_1 = Lecturer('Иван', 'Иванов')
+lecturer_2 = Lecturer('Пётр', 'Петров')
 
-print(lecturer.grades)
+reviewer = Reviewer('Сергей', 'Сергеев')
+
+student_1.courses_in_progress += ['Python', 'Git']
+student_1.finished_courses += ['Введение в программирование']
+
+student_2.courses_in_progress += ['Python']
+student_2.finished_courses += ['Введение в программирование']
+
+lecturer_1.courses_attached += ['Python']
+lecturer_2.courses_attached += ['Python']
+reviewer.courses_attached += ['Python']
+
+reviewer.rate_hw(student_1, 'Python', 10)
+reviewer.rate_hw(student_1, 'Python', 9)
+reviewer.rate_hw(student_2, 'Python', 8)
+reviewer.rate_hw(student_2, 'Python', 7)
+
+student_1.rate_lecture(lecturer_1, 'Python', 10)
+student_1.rate_lecture(lecturer_1, 'Python', 9)
+student_2.rate_lecture(lecturer_2, 'Python', 8)
+student_2.rate_lecture(lecturer_2, 'Python', 7)
+
+print(student_1)
+print()
+print(lecturer_1)
+print()
+print(reviewer)
+print()
+print(student_1 < student_2)
+print(lecturer_1 < lecturer_2)
